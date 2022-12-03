915d17c4304e43229d6ef9f56d44f7d31803c3c4
kopackib <115604429+kopackib@users.noreply.github.com>
2022-12-03 18:37:41 -0500

    Updated JS to show current temperature

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "97c5eee0fbcbc7889312010326f81709";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
